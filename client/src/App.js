d7e9658273e1c5e66af15499b1ee0ce1adb46a59
AsadKhan4491 <asadmk6293@gmail.com>
2021-07-27 00:29:39 -0700

    working on client components for Posts.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
-import {Conainer, AppBar, Typography, Grow, Grid, Container} from '@material-ui/core';
-
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import useStyles from './styles';
 import memories from './images/memories.png';
 
 const App = () => {
+    const classes = useStyles();
+
     return(
-        <Container>
-            <AppBar postion = "static" color= "inherit">
-                <Typography variant = "h2" align= "center"> Memories</Typography>
-                <img src ={memories} alt="memories" height="600" />
+        <Container maxidth="lg">
+            <AppBar className={classes.appBar} postion="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60"/>
             </AppBar>
             <Grow in>
                 <Container>
                     <Grid container justify= "space-between" alignItems = "stretch" spacing={3}>
-                        <Grid item sx={12} sm={7}>
+                        <Grid item xs={12} sm={7}>
                             <Posts />
                         </Grid>
-                        <Grid item sx={12} sm={4}>
+                        <Grid item xs={12} sm={4}>
                             <Form />
                         </Grid>
                     </Grid>
